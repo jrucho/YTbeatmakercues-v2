6b3d5e77f55be628ca8bb87bb59ea60a8ea0ea19
jrucho <carlosabeijonmartinez@gmail.com>
2026-02-01 00:36:36 +0100

    Load module entrypoint via dynamic import

--- content.js
+++ content.js
@@ -1,21 +1,6 @@
-import { createStore } from "./src/store/store.js";
-import { reducer } from "./src/store/reducer.js";
-import { initialState } from "./src/store/initial-state.js";
-import { createAppController } from "./src/core/app-controller.js";
-import { bootstrapLegacyApp, teardownLegacyApp } from "./src/ui/legacy-app.js";
-
-const store = createStore(reducer, initialState);
-
-const appController = createAppController({
-  store,
-  bootstrap: (session) => bootstrapLegacyApp(session, store),
-  teardown: () => teardownLegacyApp()
-});
-
-appController.start();
-
-const globalAbort = new AbortController();
-window.addEventListener("beforeunload", () => {
-  void appController.stop();
-  globalAbort.abort();
-}, { signal: globalAbort.signal });
+(() => {
+  const entryUrl = chrome.runtime.getURL("src/entrypoint.js");
+  import(entryUrl).catch((error) => {
+    console.error("Failed to load YT Beatmaker entrypoint:", error);
+  });
+})();
