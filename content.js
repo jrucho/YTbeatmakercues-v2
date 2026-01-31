3c6414195feb552589c2106cd36512f3bcb3d4de
jrucho <carlosabeijonmartinez@gmail.com>
2026-02-01 00:54:32 +0100

    Load entrypoint via injected module script

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
 (() => {
   const entryUrl = chrome.runtime.getURL("src/entrypoint.js");
-  import(entryUrl).catch((error) => {
+  const script = document.createElement("script");
+  script.type = "module";
+  script.src = entryUrl;
+  script.dataset.ytbmEntry = "true";
+  script.onload = () => {
+    script.remove();
+  };
+  script.onerror = (error) => {
     console.error("Failed to load YT Beatmaker entrypoint:", error);
-  });
+    script.remove();
+  };
+  (document.head || document.documentElement).appendChild(script);
 })();
